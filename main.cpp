3d71450c4b77b6b1e5ac12e95e30482a81b799b7
Cheungki <3180103162@zju.edu.cn>
2020-06-29 22:59:23 +0800

    check the basic functions

--- main.cpp
+++ main.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cxxabi.h>
-#include "tinyxml2.h"
+#include <utility>
 #include "xml_serialization.h"
-#include "convert_to_char.h"
 #include "xml_deserialization.h"
 
 using namespace std;
 
 int main()
 {
-    pair<vector<int>, pair<vector<int>, double>> temp;
-    deserializing_xml t;
-    t.deserialize_xml(temp, "std_pair", "../test.xml");
+    pair<vector<int>, pair<vector<int>, double>> temp1, temp2;
+    temp1.first = {0, 1, 2, 3, 4};
+    temp1.second.first = {5, 6, 7, 8, 9};
+    temp1.second.second = 2.33;
+    serializing_xml t1;
+    t1.serialize_xml(temp1, "std_pair", "../test.xml");
+    deserializing_xml t2;
+    t2.deserialize_xml(temp2, "std_pair", "../test.xml");
+    cout << boolalpha << (temp1 == temp2) << endl;
     return 0;
 }
