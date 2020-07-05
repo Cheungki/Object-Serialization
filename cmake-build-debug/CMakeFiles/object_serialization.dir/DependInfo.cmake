62576901efb1c975f8ab7eb756b59c3cd1b7bf68
Cheungki <3180103162@zju.edu.cn>
2020-07-05 13:00:21 +0800

    delete .idea

---
+++ cmake-build-debug/CMakeFiles/object_serialization.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhangqi/CLionProjects/object_serialization/binary_serialization.cpp" "/Users/zhangqi/CLionProjects/object_serialization/cmake-build-debug/CMakeFiles/object_serialization.dir/binary_serialization.cpp.o"
+  "/Users/zhangqi/CLionProjects/object_serialization/convert_to_char.cpp" "/Users/zhangqi/CLionProjects/object_serialization/cmake-build-debug/CMakeFiles/object_serialization.dir/convert_to_char.cpp.o"
+  "/Users/zhangqi/CLionProjects/object_serialization/main.cpp" "/Users/zhangqi/CLionProjects/object_serialization/cmake-build-debug/CMakeFiles/object_serialization.dir/main.cpp.o"
+  "/Users/zhangqi/CLionProjects/object_serialization/tinyxml2.cpp" "/Users/zhangqi/CLionProjects/object_serialization/cmake-build-debug/CMakeFiles/object_serialization.dir/tinyxml2.cpp.o"
+  "/Users/zhangqi/CLionProjects/object_serialization/xml_serialization.cpp" "/Users/zhangqi/CLionProjects/object_serialization/cmake-build-debug/CMakeFiles/object_serialization.dir/xml_serialization.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
